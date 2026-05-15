e21ee66554ec574a55e3de47b0e7c2d7cb2569f7
Az <103241502+HereLiesAz@users.noreply.github.com>
2026-05-15 10:39:08 -0500

    Initialise AuditLogger from DoxrayApp + log app start

--- app/src/main/java/com/hereliesaz/doxray/DoxrayApp.kt
+++ app/src/main/java/com/hereliesaz/doxray/DoxrayApp.kt
@@ -1,11 +1,16 @@
 package com.hereliesaz.doxray
 
 import android.app.Application
+import com.hereliesaz.doxray.audit.AuditLogger
+import com.hereliesaz.doxray.db.AppDatabase
 import com.hereliesaz.doxray.net.HttpClients
 
 class DoxrayApp : Application() {
     override fun onCreate() {
         super.onCreate()
         HttpClients.init(this)
+        val db = AppDatabase.getDatabase(this)
+        AuditLogger.init(db.auditDao())
+        AuditLogger.log(AuditLogger.Type.LIFECYCLE, "App started")
     }
 }
